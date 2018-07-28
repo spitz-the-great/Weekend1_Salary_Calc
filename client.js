bca01ed0ed59eeb935696c2e5a5585a1cc880ba0
spitz-the-great <jspitzley85@gmail.com>
2018-07-28 15:31:31 -0500

    updated so the array and row stay gone for good

--- client.js
+++ client.js
@@ -6,10 +6,15 @@ function readyNow() {
     console.log('jq')
     $('.submitButton').on('click', addEmp);
     $('#totalLine').html('<div id="totalLine">Monthly Total: $0 </div>')
-    $('#tableActual').on('click', '.deleteButton',  removeEmp);
+    $('#tableActual').on('click', '.deleteButton', removeEmp);
 } // end readyNow
 
+
 employees = [];
+let monthlyTotal = 0;
+let arrayIndex = 0;
+// end global vars
+
 
 class Employee {
     constructor(firstName, lastName, ID, title, empSalary) {
@@ -21,13 +26,36 @@ class Employee {
     }
 } // end Employee
 
-let monthlyTotal = 0;
 
-function removeEmp(){
+function removeEmp() {
     console.log('in removeEmp');
-    $(this).closest('tr').remove();  
+    idTag = $(this).attr('id');
+    $(this).closest('tr').empty();
+    $(this).closest('tr').remove();
+    removeById(idTag, employees);
+    console.log(idTag);
     return true;
 }
+
+
+function removeById(ID, array){
+    console.log('in removeById');
+    for ( i = 0; i< array.length; i++){
+        if( array[i].ID == ID){
+            array.splice(i,1);
+        }
+    }
+}
+
+function dupSearch(ID, array) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i].ID == ID) {
+            return true;
+        }
+        else return false;
+    } // takes the employee.ID entered in the input and searches array for duplicate
+} // end dupSearch
+
 
 function addEmp() {
     let fName = $('#firstNameInput').val();
@@ -35,14 +63,22 @@ function addEmp() {
     let iD = $('#idInput').val();
     let title = $('#empTitleInput').val();
     let salary = parseInt($('#salaryInput').val());
-    newEmp(fName, lName, iD, title, salary);
+    if (dupSearch(iD, employees) === true) {
+        alert("Duplicate ID");
+    }
+    else {
+        newEmp(fName, lName, iD, title, salary);
+    }
 } // end addEmp
+
 
 function newEmp(fName, lName, iD, title, salary) {
     employees.push(new Employee(fName, lName, iD, title, salary));
+
     updateEmpTable();
     updateTotal();
 } // end newEmp
+
 
 function updateEmpTable() {
     $('#tableActual').empty();
@@ -54,15 +90,20 @@ function updateEmpTable() {
             + employee.ID + '</td><td>'
             + employee.title + '</td><td>'
             + employee.empSalary + '</td><td>'
-            +'<button class="deleteButton">Delete</button></td></tr>');
+            + '<button id="' + employee.ID + '" class="deleteButton">Delete</button></td></tr>');
+
+
     }
+    console.log(employees[employees.length - 1]);
 } // end updateEmpTable
+
 
 function updateTotal() {
     monthlyTotal += parseInt($('#salaryInput').val());
     $('#totalLine').html('<div id="totalLine">Monthly Total: $' + monthlyTotal + '</div>');
     overCap();
 } // end updateTotal
+
 
 function overCap() {
     if (monthlyTotal > 20000) {
